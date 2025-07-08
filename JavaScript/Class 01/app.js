05ea3c86b36ccd7a59e2e39ea1c9da0f766b4385
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-07-08 10:42:17 +0500

    Alert / Variables

--- JavaScript/Class 01/app.js
+++ JavaScript/Class 01/app.js
@@ -1,5 +1,42 @@
 // JavaScript is Casa Sensative
 
+// alert("Hello Saylani Students");
 
+// Data Type
+// 232332          => Number
+// true            => Boolean
+// "Adil Ahmed"    => String
+
+// window.alert()
+
+// Variables  => For Managing Data (For Store Data)
+
+// var fullName = "Adil Ahmed"  => Create Variable (Var Keyword)
+
+// alert(fullName); => Get Variables Values
+
+// Update Variable Value
+
+// fullName = "Ali";
+
+// Undefined Variable (False Value)
+
+// fullName;
+
+// Cases in JavaScript
+
+// myFullName => Camel Case
+// my_full_name => Snake Case
+
+// Variable in Number
+
+// var num = 10;
+
+// alert(num);
+
+// var num = 10;
+// var inc = num + 10;
+
+// alert(inc);
 
 
