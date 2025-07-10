80998bcbb8a91d4df3d4c7f2f034150ade0b0a42
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-10 20:29:34 +0500

    Variable names

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,27 @@
+alert("Hello Students");
+
+// Legal and Illegal Name of Variable
+
+
+// Illegal Name
+// var 1st = "abc" ; => Number Start
+// var alert = "Abc " => Predefined Keyword
+
+// Legal Name
+// var num1 = 123;
+// var full_name = "Adil "
+
+// For Cade Debuging
+// console.log("Adil Ahmed");
+
+// Math Expressions
+// Addition = +
+// Subtraction = -
+// Multiplication = *
+// Division = /
+// Remainder = %
+
+// var num = 10;
+
+// console.log(num % 10)
+
