4f2e1a5f3e530df046405a8d0acc71fed072aca0
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-07-19 12:39:38 +0500

    Prompt

--- JavaScript/Class 05/app.js
+++ JavaScript/Class 05/app.js
@@ -1,1 +1,18 @@
 console.log("Hello Students");
+
+// Prompts
+
+var spec = prompt("Your species?", "human");
+
+//  Received Input From User
+
+// Place Holder and Default Value
+
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+// Increment using Prompts
+
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
