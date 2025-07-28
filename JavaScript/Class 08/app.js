646712a66aadbcd224fd86e049f4af30716d0cc4
AdilAhmedShekhani <adilaziz2009@live.com>
2025-07-28 22:01:10 +0500

    Arrays

--- JavaScript/Class 08/app.js
+++ JavaScript/Class 08/app.js
@@ -1,2 +1,26 @@
 console.log("Hello Students");
 
+var student1 = "Ali"
+var student2 = "Akif"
+var student3 = "Hammad"
+
+console.log(student1)
+console.log(student2)
+console.log(student3)
+
+
+// Arrays
+
+var students = ["Ali", "Akif", "Hammad"]
+
+console.log(students[0])
+
+// Array multiple index call
+
+console.log(students[0], students[1])
+
+// Arrays Math Expression
+
+var num = [1, 2, 3, 4, 5]
+
+console.log(num[1] + num[3])
