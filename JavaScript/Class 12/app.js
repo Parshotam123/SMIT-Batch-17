1e1a5ebf1b783be76cf15e5ff49a538a6a679fa6
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-08-07 10:08:30 +0500

    Break and Flag

--- JavaScript/Class 12/app.js
+++ JavaScript/Class 12/app.js
@@ -1,1 +1,28 @@
 alert("Hello");
+
+// Break
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i === 5) {
+    break;
+  }
+}
+
+// Flag Variable Use
+
+let numbers = [10, 20, 30, 40, 50];
+let Flag = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] === 30) {
+    Flag = true;
+    break;
+  }
+}
+
+if (Flag) {
+  console.log("Number mil gaya!");
+} else {
+  console.log("Number nahi mila.");
+}
