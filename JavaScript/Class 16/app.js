dc8511d55064543518c282cf5ad26d998254320c
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-08-28 09:45:02 +0500

    Convert String to Numbers

--- JavaScript/Class 16/app.js
+++ JavaScript/Class 16/app.js
@@ -1,1 +1,12 @@
 console.log("Hello");
+
+// Convert String to Numbers
+// Example 1
+var integerString = "24";
+var num = Number(integerString);
+
+// Example 2
+var floatingNumString = "24.9876";
+var num = Number(floatingNumString);
+
+
