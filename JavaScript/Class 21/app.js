65a5f7b63acc49b3d64933151fadb4e2b24ddfe3
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-09-16 10:20:38 +0500

    Switch Statement Example

--- JavaScript/Class 21/app.js
+++ JavaScript/Class 21/app.js
@@ -24,3 +24,17 @@ switch (dayOfWk) {
   default:
     alert("Shoot me now!");
 }
+
+// Example switch statement
+var day = "Monday";
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday");
+    break;
+  default:
+    console.log("Today is neither Monday nor Tuesday");
+    break;
+}
