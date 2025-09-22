581fa9eb17057c9d03fb9cdac65f70e9f54cbe7d
AdilAhmedShekhani <adilaziz2009@live.com>
2025-09-22 21:40:00 +0500

    Attendance Checker

--- JavaScript/Class 22/app.js
+++ JavaScript/Class 22/app.js
@@ -1,1 +1,15 @@
 console.log("Hello")
+
+// Attendance Checker
+
+function checkAttendance(student) {
+    var lastClass = [1122, 2233, 3344];
+    if (lastClass.indexOf(student) !== -1) {
+        return "Present"
+    } else {
+        return "Absent"
+    }
+}
+
+console.log(checkAttendance(2233))
+
