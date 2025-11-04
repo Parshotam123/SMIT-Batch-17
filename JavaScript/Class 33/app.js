ffc38c87a2472aa2494b29f9a2e8fdb790186232
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2025-11-04 09:56:07 +0500

    Objects Methods

--- JavaScript/Class 33/app.js
+++ JavaScript/Class 33/app.js
@@ -1,1 +1,28 @@
-console.log("Hello");
+// console.log("Hello");
+
+// Methods in Objects
+
+var plan1 = {
+  name: "Basic",
+  price: 3.99,
+  space: 100,
+  transfer: 1000,
+  pages: 10,
+  discountMonths: [6, 7],
+  calcAnnual: function (percentIfDisc) {
+    var bestPrice = plan1.price;
+    var currDate = new Date();
+    var thisMo = currDate.getMonth();
+    for (var i = 0; i < plan1.discountMonths.length; i++) {
+      if (plan1.discountMonths[i] === thisMo) {
+        bestPrice = plan1.price * percentIfDisc;
+        break;
+      }
+    }
+    return bestPrice * 12;
+  },
+};
+
+var annualPrice = plan1.calcAnnual(0.85);
+
+console.log(annualPrice);
