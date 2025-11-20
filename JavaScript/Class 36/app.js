5b320c1c00dcced7aee4816d5155561c5463f53e
AdilAhmedShekhani <adilaziz2009@live.com>
2025-11-20 21:11:06 +0500

    Try & Catch

---
+++ JavaScript/Class 36/app.js
@@ -0,0 +1,16 @@
+// function greetWorld() {
+//     var greeting = "Hello world!";
+//     aler(greeting);
+// }
+
+// Using Try and Catch
+
+function greetWorld() {
+    try {
+        var greeting = "Hello world!";
+        aler(greeting);
+    }
+    catch (err) {
+        alert(err);
+    }
+}
