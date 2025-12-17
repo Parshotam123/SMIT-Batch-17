f64cfce3748606eaaec573d9d8283c200d046c50
AdilAhmedShekhani <adilaziz2009@live.com>
2025-12-17 20:33:47 +0500

    Encapsulation

--- Advance Javascript/Class 06/app.js
+++ Advance Javascript/Class 06/app.js
@@ -1,12 +1,26 @@
 // Javascript OOP Concepts
 
 class Student {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  study() {
-    console.log(this.name + " is studying");
-  }
+    study() {
+        console.log(this.name + " is studying");
+    }
+}
+
+// OOP Concepts Javascript
+
+// Encapsulation
+
+class BankAccount {
+    constructor(balance) {
+        this.balance = balance;
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
 }
